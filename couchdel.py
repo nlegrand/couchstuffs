9346e8621447e54f622f9e9fb6f2af882893aa4d
Nicolas P. M. Legrand <nlegrand@ethelred.fr>
2010-12-01 15:00:33 +0100

    No safety net for couchdel
    I just want to del as I rm in UNIX. couchdel will get the current
    document revision or check if it's a database and delete the document
    or database.

--- couchdel.py
+++ couchdel.py
@@ -14,7 +14,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import sys, httplib, getopt, ConfigParser, os
+import sys, httplib, getopt, ConfigParser, os, json
 
 opts, args = getopt.getopt(sys.argv[1:], 'h:p:s:c:')
 
@@ -49,12 +49,37 @@ if not host:
 if not port:
     port = config.get(config_section, 'port')
 
+rev_connexion = httplib.HTTPConnection(host, port)
+rev_connexion.request("GET", args[0])
+rev_response = rev_connexion.getresponse()
+rev_response_output = rev_response.read()
+if rev_response.status != 200:
+    sys.stderr.write(str(rev_response.status) + " "
+                     + rev_response.reason + "\n")
+    sys.stdout.write(rev_response_output)
+    sys.exit(1)
+try:
+    doc = json.loads(rev_response_output)
+except ValueError:
+    sys.stderr.write("http://" + host + ":" + "port" + args[0]
+                     + " answer doesn't seem to be JSON\n")
+    sys.exit(1)
+    
+
+#check if doc is a ressource or a database
+if '_rev' in doc:
+    url_arg = "?rev=" + doc['_rev']
+elif 'db_name' in doc:
+    url_arg = ""
+else:
+    sys.stderr.write("cannot get _rev from ressource:"
+                     + host + ":" + port + "/" + args[0] + "\n")
+    sys.exit(1)
+
 connexion = httplib.HTTPConnection(host, port)
 
-connexion.request("DELETE", args[0])
+connexion.request("DELETE", args[0] + url_arg)
 response = connexion.getresponse()
 sys.stderr.write(str(response.status) + " "
                  + response.reason + "\n")
 sys.stdout.write(response.read())
-
-
